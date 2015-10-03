2ad789db7061fb86af44d38fa95d3f49fe99306f
fa7labs <fa7labs@FA7-L40M19.local>
2015-10-03 12:33:30 -0300

    commit projeto

--- Quiz_ENADE/Quiz_ENADE/ViewController.swift
+++ Quiz_ENADE/Quiz_ENADE/ViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var lbQuestion: UILabel!
+    @IBOutlet weak var imgQuestion: UIImageView!
+    @IBOutlet weak var btnAnswer1: UIButton!
+    @IBOutlet weak var btnAnswer2: UIButton!
+    @IBOutlet weak var btnAnswer3: UIButton!
+    
+    @IBOutlet weak var viewFeedback: UIView!
+    @IBOutlet weak var lbFeedback: UILabel!
+    @IBOutlet weak var btnFeedback: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +31,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func chooseAnswer1(sender: AnyObject) {
+    }
+    
+    @IBAction func chooseAnswer2(sender: AnyObject) {
+    }
+    
+    @IBAction func chooseAnswer3(sender: AnyObject) {
+    }
+    
+    @IBAction func btnFeedback(sender: AnyObject) {
+    }
 
 }
 
