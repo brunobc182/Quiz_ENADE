2049ed0ba2c156d51143f01e39a359179abd4f13
fa7labs <fa7labs@FA7-L40M10-2.local>
2015-10-03 12:35:48 -0300

    Tela e funçoes

--- Quiz_ENADE/Quiz_ENADE/ViewController.swift
+++ Quiz_ENADE/Quiz_ENADE/ViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lbQuestion: UILabel!
+    @IBOutlet weak var imgQuestion: UIImageView!
+    @IBOutlet weak var btnAnswer1: UIButton!
+    @IBOutlet weak var btnAnswer2: UIButton!
+    @IBOutlet weak var btnAnswer3: UIButton!
+    
+    
+    @IBOutlet weak var viewFeedback: UIView!
+    @IBOutlet weak var lbFeedback: UILabel!
+    @IBOutlet weak var btnFeedback: UIButton!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +33,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func chooseAnswer1(sender: AnyObject) {
+    }
+    @IBAction func chooseAnswer2(sender: AnyObject) {
+    }    
+    @IBAction func chooseAnswer3(sender: AnyObject) {
+    }
+    @IBAction func btnFeedback(sender: AnyObject) {
     }
 
 
